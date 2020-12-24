84da4bca6fe6f9f092d66b23eb0733c00bc5834a
Aniket762 <aniketindian8@gmail.com>
2020-12-24 16:10:53 +0530

    Tindercards

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import TinderCards from "./TinderCards";
+
+function App() {
+  return (
+    <div className="app">
+    <Header />
+    <TinderCards />
+    
+    {/*SwipeButton*/}
+    </div>
+  );
+}
+
+export default App;
